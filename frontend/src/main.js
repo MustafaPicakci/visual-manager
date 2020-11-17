26bcc3920b61a2a07d79f4d9506e7d42374e02e0
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-11-17 23:47:12 +0300

    frontend checkpoint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { router } from "./router";
+import store from "./store";
 
+import Vuex from "vuex";
+/*
+
+Vue.config.productionTip = false;
+
+
+
+*/
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
+
+Vue.use(Vuex);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 new Vue({
-  el: '#app',
+  router,
+  store,
   render: h => h(App)
-})
+}).$mount("#app");
---
+++ frontend/src/router.js
@@ -0,0 +1,81 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+import Profile from "./views/Profile.vue";
+import BoardAdmin from "./views/BoardAdmin.vue";
+import BoardModerator from "./views/BoardModerator.vue";
+import BoardUser from "./views/BoardUser.vue";
+import store from "./store/index";
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+      beforeEnter(to, from, next) {
+        
+        if (store.getters.isLoggedIn) {
+          next();
+        } else {
+          next("/login");
+        }
+      }
+    },
+    {
+      path: "/home",
+      component: Home
+    },
+    {
+      path: "/login",
+      component: Login
+    },
+    {
+      path: "/register",
+      component: Register
+    },
+    {
+      path: "/profile",
+      name: "profile",
+
+      component: Profile
+    },
+    {
+      path: "/admin",
+      name: "admin",
+
+      component: BoardAdmin
+    },
+    {
+      path: "/mod",
+      name: "moderator",
+
+      component: BoardModerator
+    },
+    {
+      path: "/user",
+      name: "user",
+
+      component: BoardUser
+    }
+  ]
+});
+
+// router.beforeEach((to, from, next) => {
+//   const publicPages = ['/login', '/register', '/home'];
+//   const authRequired = !publicPages.includes(to.path);
+//   const loggedIn = localStorage.getItem('user');
+
+//   // trying to access a restricted page + not logged in
+//   // redirect to login page
+//   if (authRequired && !loggedIn) {
+//     next('/login');
+//   } else {
+//     next();
+//   }
+// });
