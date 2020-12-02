323aff1bc730a7fca3002561dd103a41fdcb4bb3
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-12-02 23:46:42 +0300

    tag tabloları eklendi

---
+++ backend/src/main/java/com/application/backend/models/Tags.java
@@ -0,0 +1,43 @@
+package com.application.backend.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tags")
+public class Tags {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private long tag_id;
+
+  public long getTag_id() {
+    return tag_id;
+  }
+
+  public void setTag_id(long tag_id) {
+    this.tag_id = tag_id;
+  }
+
+  public String getTag_name() {
+    return tag_name;
+  }
+
+  public void setTag_name(String tag_name) {
+    this.tag_name = tag_name;
+  }
+
+  @Column(unique = true)
+  private String tag_name;
+}
