780dcb12b66889f5e32580309a90d70540c1704c
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-12-24 11:29:26 +0300

    dropzone opacity değişikliği

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,21 +2,24 @@
   <div class="container text-center">
     <Loader :status="loaderStatus"></Loader>
     <br />
-    <vue-dropzone
-      :options="dropzoneOptions"
-      :useCustomSlot="true"
-      @vdropzone-success="vsuccess"
-      id="dropzone"
-    >
-      <div class="dropzone-custom-content">
-        <h3 class="dropzone-custom-title">
-          Görselleri Buraya sürükle ve bırak !
-        </h3>
-        <div class="subtitle">
-          ...veya sadece tıkla ve istediğin görseli seç
+    <div class="dropzone">
+      <vue-dropzone
+        :options="dropzoneOptions"
+        :useCustomSlot="true"
+        :include-styling="false"
+        @vdropzone-success="vsuccess"
+        id="dropzone"
+      >
+        <div class="dropzone-custom-content ">
+          <h3 class="dropzone-custom-title">
+            Görselleri Buraya sürükle ve bırak !
+          </h3>
+          <div class="subtitle">
+            ...veya sadece tıkla ve istediğin görseli seç
+          </div>
         </div>
-      </div>
-    </vue-dropzone>
+      </vue-dropzone>
+    </div>
     <br />
     <br />
     <br />
@@ -54,9 +57,10 @@ export default {
       pageSize: 20,
       dropzoneOptions: {
         url: "http://localhost:3000/api/images/add",
-        maxFilesize:45.0,
+        maxFilesize: 45.0,
         thumbnailWidth: 200,
         addRemoveLinks: true,
+
         headers: { ...authHeader() }
       }
     };
@@ -81,7 +85,7 @@ export default {
       if (pageSize) {
         params["pageSize"] = pageSize;
       }
-      this.loaderStatus= true;
+      this.loaderStatus = true;
       return params;
     },
     retrieveImages() {
@@ -113,5 +117,17 @@ export default {
 
 .subtitle {
   color: #314b5f;
+}
+
+
+#dropzone {
+  position: relative;
+  z-index: 1;
+  background: rgba(155, 211, 156, 0.08);
+
+  height: 200px;
+  
+  border:5 px solid  rgba(155, 211, 156, 0.1);
+  border-radius: 15px;
 }
 </style>
