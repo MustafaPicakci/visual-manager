308c290f01d4d78d352a19143ca8167cf6471c36
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2020-12-27 01:46:28 +0300

    kullanıcı adı değiştirme sorunu çözüldü

--- backend/src/main/java/com/application/backend/controllers/UserController.java
+++ backend/src/main/java/com/application/backend/controllers/UserController.java
@@ -1,17 +1,25 @@
 package com.application.backend.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.application.backend.models.User;
-import com.application.backend.repository.UserRepository;
+import com.application.backend.payload.response.JwtResponse;
+import com.application.backend.security.jwt.JwtUtils;
+import com.application.backend.security.services.UserDetailsImpl;
 import com.application.backend.security.services.UserDetailsServiceImpl;
 
 @CrossOrigin(origins = "*")
@@ -19,16 +27,37 @@ import com.application.backend.security.services.UserDetailsServiceImpl;
 @RequestMapping("/api/user")
 public class UserController {
   @Autowired UserDetailsServiceImpl userDetailServiceImpl;
+  @Autowired AuthenticationManager authenticationManager;
+  @Autowired JwtUtils jwtUtils;
 
   @PostMapping("/change/username")
   @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-  public ResponseEntity<User> changeUserName(User user) {
-    try {
-      userDetailServiceImpl.changeUserName(user.getId(), user.getUsername());
+  public ResponseEntity<JwtResponse> changeUserName(User user) {
 
-      return new ResponseEntity<>(userDetailServiceImpl.loadUserById(user.getId()), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    userDetailServiceImpl.changeUserName(user.getId(), user.getUsername());
+
+    Authentication authentication =
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    String jwt = jwtUtils.generateJwtToken(authentication);
+    Long expirationDate = jwtUtils.getExpitarionDate();
+    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+    List<String> roles =
+        userDetails
+            .getAuthorities()
+            .stream()
+            .map(item -> item.getAuthority())
+            .collect(Collectors.toList());
+
+    return ResponseEntity.ok(
+        new JwtResponse(
+            expirationDate,
+            jwt,
+            userDetails.getId(),
+            userDetails.getUsername(),
+            userDetails.getEmail(),
+            roles));
   }
 }
