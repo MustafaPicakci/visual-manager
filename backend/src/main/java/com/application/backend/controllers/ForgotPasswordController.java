7b2df21a5ac629db80fa74097f9d7beafe097f3a
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2021-01-03 21:30:07 +0300

    kullanılmayan importlar silindi

--- backend/src/main/java/com/application/backend/controllers/ForgotPasswordController.java
+++ backend/src/main/java/com/application/backend/controllers/ForgotPasswordController.java
@@ -5,7 +5,6 @@ import javax.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- backend/src/main/java/com/application/backend/repository/TagRepository.java
+++ backend/src/main/java/com/application/backend/repository/TagRepository.java
@@ -3,8 +3,6 @@ package com.application.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.application.backend.models.Images;
@@ -15,13 +13,6 @@ public interface TagRepository extends JpaRepository<Tags, Long> {
   Tags findTagsByTagName(String tagName);
 
   Tags findTagsById(long tagId);
-  public List<Tags> findTagsByimages(Images image);
-  
-  /*
-  @Query("select count tag_id from image_tags where tag_id ")
-  public boolean isUsed(
-		  @Param("product_id") int product_id)
-  /* List<Tags> findAllByUser(User user);
 
-  void deleteByUser(User user);*/
+  public List<Tags> findTagsByimages(Images image);
 }
