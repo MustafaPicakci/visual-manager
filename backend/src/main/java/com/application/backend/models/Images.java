1c6cb3dc1b6537702d1e527400a2a2c51ab12dbf
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2021-01-12 01:02:40 +0300

    hesap silme asenkron hale getirildi(işlemin tamamlandığının geri dönüsü alınamıyor), login/register sayfa titremesi düzeltildi

--- backend/src/main/java/com/application/backend/models/Images.java
+++ backend/src/main/java/com/application/backend/models/Images.java
@@ -1,10 +1,8 @@
 package com.application.backend.models;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
