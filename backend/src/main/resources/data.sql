3c60303358e054de26959d4afc05486fee9686e7
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2021-04-01 01:08:35 +0300

    data.sql updated

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,8 +7,8 @@ SELECT 'ROLE_USER'
 WHERE NOT EXISTS (SELECT * FROM roles WHERE name='ROLE_USER');
 
 INSERT INTO users (email,is_active,password,username)
-select 'mustafapicakci25@gmail.com',true,'$2a$10$duud6MSgUNpQQoS7nsvB7eD1JB8WjTQNFrfU8A4aoYSM96n109RKq','mustafa'
-WHERE NOT EXISTS (SELECT * FROM users WHERE username='mustafa');
+select 'info.VisualManager2@gmail.com',true,'$2a$10$duud6MSgUNpQQoS7nsvB7eD1JB8WjTQNFrfU8A4aoYSM96n109RKq','adminn'
+WHERE NOT EXISTS (SELECT * FROM users WHERE username='adminn');
 
 INSERT INTO user_roles (user_id,role_id)
 select 1,1
